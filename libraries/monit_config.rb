c4398098c2bf59bd7a3b92f43195a3f62fceedea
Anthony Caiafa <acaiafa1@bloomberg.net>
2015-10-22 10:58:10 -0400

    updating library

--- libraries/monit_config.rb
+++ libraries/monit_config.rb
@@ -5,75 +5,78 @@
 # Copyright (C) 2015 Bloomberg Finance L.P.
 require 'poise_service/service_mixin'
 
-class Chef::Resource::MonitConfig < Chef::Resource
-  include Poise
-  provides(:monit_config)
-  include PoiseService::ServiceMixin
+module MonitCookbook
+  module Resource
+    class MonitConfig < Chef::Resource     
+      include Poise
+      provides(:monit_config)
+      include PoiseService::ServiceMixin
 
-  actions(:enable, :disable)
-  default_action(:enable)
+      # @!attribute instance_name
+      # @return [String]
+      attribute(:instance_name, kind_of: String, :name_attribute => true)
 
-  # @!attribute instance_name
-  # @return [String]
-  attribute(:instance_name, kind_of: String, :name_attribute => true)
+      # @see: config options for all things monit
+      attribute(:pkg, kind_of: String, default: 'monit')
+      attribute(:check, kind_of: String)
+      attribute(:with, kind_of: String)
+      attribute(:group, kind_of: String)
+      attribute(:start_program, kind_of: String)
+      attribute(:stop_program, kind_of: String)
+      attribute(:mode, kind_of: String)
+      attribute(:user, kind_of: String)
+      attribute(:every, kind_of: String)
+      attribute(:test_conditions, kind_of: Array)
 
-  # @see: config options for all things monit
-  attribute(:pkg, kind_of: String, default: 'monit')
-  attribute(:check, kind_of: String)
-  attribute(:with, kind_of: String)
-  attribute(:group, kind_of: String)
-  attribute(:start_program, kind_of: String)
-  attribute(:stop_program, kind_of: String)
-  attribute(:mode, kind_of: String)
-  attribute(:user, kind_of: String)
-  attribute(:every, kind_of: String)
-  attribute(:test_conditions, kind_of: Array)
-end
+    end
+  end
 
-class Chef::Provider::MonitConfig < Chef::Provider
-  include Poise
-  provides(:monit_config)
-  include PoiseService::ServiceMixin
+  module Provider
+    class MonitConfig < Chef::Provider
+      include Poise
+      include PoiseService::ServiceMixin
+      provides(:monit_config)
 
-  # Create monit files
-  def action_enable
-    notifying_block do
-      # Install package
-      package new_resource.pkg do
-        package_name new_resource.pkg
-        action :upgrade
+      # Create monit files
+      def action_enable
+        notifying_block do
+          # Install package
+          package new_resource.pkg do
+            package_name new_resource.pkg
+            action :upgrade
+          end
+
+          template "/etc/monit/conf.d/#{new_resource.instance_name}" do
+            cookbook "monit"
+            source "monit_check.erb"
+            owner "root"
+            group "root"
+            variables(
+              config: new_resource,
+            )
+          end
+
+          file "/etc/init.d/monit" do
+            action :delete
+          end
+        end
+        super
       end
 
-      template "/etc/monit/conf.d/#{new_resource.instance_name}" do
-        cookbook "monit"
-        source "monit_check.erb"
-        owner "root"
-        group "root"
-        variables(
-          config: new_resource,
-        )
+      def action_delete
+        notifying_block do
+          file "/etc/monit/conf.d/#{new_resource.instance_name}" do
+            action :delete
+          end
+        end
+        super
       end
 
-      file "/etc/init.d/monit" do
-        action :delete
+      def service_options(service)
+        service.service_name('monit')
+        service.command('/usr/bin/monit -c /etc/monit/monitrc')
+        service.restart_on_update(true)
       end
     end
-    super
-  end
-
-  def action_delete
-    notifying_block do
-      file "/etc/monit/conf.d/#{new_resource.instance_name}" do
-        action :delete
-      end
-    end
-    super
-  end
-
-  def service_options(service)
-    service.service_name('monit')
-    service.command('/usr/bin/monit -c /etc/monit/monitrc')
-    service.restart_on_update(true)
   end
 end
-
